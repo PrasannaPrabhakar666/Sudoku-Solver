26716cd4a8c654e0cb6b60a0f5deb2d01e2a2e40
PrasannaPrabhakar666 <prasannaprabhakar666@gmail.com>
2022-09-08 13:55:03 +0530

    Add files via upload

---
+++ SudokuSolver.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+bool CheckisValid(vector < vector < char >> & board, int row, int column, char value) {
+  for (int iterator = 0; iterator < 9; iterator++) {
+    if (board[iterator][column] == value)
+      return false;
+
+    if (board[row][iterator] == value)
+      return false;
+
+    if (board[3 * (row / 3) + iterator / 3][3 * (column / 3) + iterator % 3] == value)
+      return false;
+  }
+  return true;
+}
+
+bool SudokuSolver(vector < vector < char >> & board) {
+  for (int row = 0; row < board.size(); row++) {
+    for (int column = 0; column < board[0].size(); column++) {
+      if (board[row][column] == '.') {
+        for (char character = '1'; character <= '9'; character++) {
+          if (CheckisValid(board, row, column, character)) {
+            board[row][column] = character;
+
+            if (SudokuSolver(board))
+              return true;
+            else
+              board[row][column] = '.';
+          }
+        }
+
+        return false;
+      }
+    }
+  }
+  return true;
+}
+int main() {
+    cout<<"Enter the values in sudoku puzzle"<<endl;
+    
+    vector<vector<char>>board(9,vector<char>(9));/*{
+        {'9', '5', '7', '.', '1', '3', '.', '8', '4'},
+        {'4', '8', '3', '.', '5', '7', '1', '.', '6'},
+        {'.', '1', '2', '.', '4', '9', '5', '3', '7'},
+        {'1', '7', '.', '3', '.', '4', '9', '.', '2'},
+        {'5', '.', '4', '9', '7', '.', '3', '6', '.'},
+        {'3', '.', '9', '5', '.', '8', '7', '.', '1'},
+        {'8', '4', '5', '7', '9', '.', '6', '1', '3'},
+        {'.', '9', '1', '.', '3', '6', '.', '7', '5'},
+        {'7', '.', '6', '1', '8', '5', '4', '.', '9'}
+    };*/
+    for (int i = 0; i < 9; i++)
+    {
+        cout<<"Enter the values of the "<<i<<" row"<<endl;
+        for(int j=0;j<9;j++)
+        {
+            cin>>board[i][j];
+        }
+    }
+    
+   
+    SudokuSolver(board);
+    cout<<"The final sudoku is :"<<endl;    	
+    for(int i= 0; i< 9; i++){
+        for(int j= 0; j< 9; j++)
+            cout<<board[i][j]<<" ";
+            cout<<"\n";
+    }
+    return 0;
+}
